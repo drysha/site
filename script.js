40ec05a147c94c6cb7fe2e02bbee4d7664ce00bf
drysha <41706244+drysha@users.noreply.github.com>
2021-05-22 16:59:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var currency = document.getElementById('currency')
+
+function changeСurrency(element){
+	if (element.innerHTML == "USD"){
+		element.innerHTML = "EU";
+	}else{
+		element.innerHTML = "USD";
+	}
+	}
